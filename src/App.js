f22becdfaa25b02f0433db7831462dd470664c5c
leejg3144 <jaosn0620@gmail.com>
2020-10-26 06:56:28 +0900

    api 연동 및

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-//import PropTypes from "prop-types";
-
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
-  }
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+    console.log(movies);
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        { isLoading ? "Loading..." : "We are ready" }
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
 
 export default App;
- 
